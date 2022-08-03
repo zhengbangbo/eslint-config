d34cc45760a4df8b163b21c938f00f833c5842ff
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-08-03 10:47:34 +0800

    feat(all): init

---
+++ packages/all/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@zhengbangbo/eslint-config-basic',
+  ],
+}
--- packages/basic/index.js
+++ packages/basic/index.js
@@ -34,7 +34,6 @@ module.exports = {
   plugins: [
     'html',
     'unicorn',
-    'zhengbangbo',
   ],
   settings: {
     'import/resolver': {
